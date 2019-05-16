925907029abb5f7fe94d530c6635ed2ce038d0ec
Ho Quang Nam <hoquangnam45@gmail.com>
2019-05-16 11:03:26 +0700

    Fixed gravity

--- pong_test.cpp
+++ pong_test.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 #include <cmath>
 #include <unistd.h>
-
-#include "Libs/Gyro/SFE_LSM9DS0.h"
-#include "Libs/OLED/oled/Edison_OLED.h"
-#include "Libs/OLED/gpio/gpio.h"
+#include <iomanip>
 
 #define G_FORCE 9.8
 #define DELTA_TIME_IN_US 1e6
@@ -12,8 +9,14 @@
 #define BALL_RADIUS 2
 #define OUTER_OFFSET 64
 #define PI 3.14
-#define POWER_LOSS_RATIO 1
+#define POWER_LOSS_RATIO 0.8
 
+int getLCDHeight() {
+    return 32;
+}
+int getLCDWidth() {
+    return 64;
+}
 using namespace std;
 
 float **listPos;
@@ -21,21 +24,6 @@ float **listVel;
 char *boardBuffer;
 int ballCount;
 int LCDWidthByBall, LCDHeightByBall, LCDAreaByBall;
-
-LSM9DS0 *imu;
-// Define an edOLED object:
-edOLED oled;
-
-// Pin definitions:
-// All buttons have pull-up resistors on-board, so just declare
-// them as regular INPUT's
-gpio BUTTON_UP(47, INPUT);
-gpio BUTTON_DOWN(44, INPUT);
-gpio BUTTON_LEFT(165, INPUT);
-gpio BUTTON_RIGHT(45, INPUT);
-gpio BUTTON_SELECT(48, INPUT);
-gpio BUTTON_A(49, INPUT);
-gpio BUTTON_B(46, INPUT);
 
 int flatIndex(int x, int y);
 int getX(int flatIndex);
@@ -46,12 +34,12 @@ void getBallCount();
 void drawingBoard();
 void movingBall();
 void updateBoard();
-
+void clear();
 void updateVelPos(float accelX, float accelY, float accelZ);
 void detectCollision();
 
 int main(){
-    initOled();
+    //initOled();
     getBallCount();
 
     listPos = new float* [ballCount];
@@ -60,68 +48,47 @@ int main(){
         listPos[i] = new float[2];
         listVel[i] = new float[2]{0};
     }
-    boardBuffer = new char[oled.getLCDHeight() * oled.getLCDWidth()];
+    boardBuffer = new char[getLCDHeight() * getLCDWidth()];
 
     initPos();
     while(true){
         updateBoard();
         drawingBoard();
         movingBall();
+        usleep(1e6);
         //usleep(DELTA_TIME_IN_US * 1e-3);
     }
     return 0;
 }
 
-void initOled(){
-    oled.begin();
-    oled.clear(PAGE);
-    oled.display();
-    oled.setFontType(0);
-}
+// void initOled(){
+//     oled.begin();
+//     oled.clear(PAGE);
+//     oled.display();
+//     oled.setFontType(0);
+// }
 
 int flatIndex(int x, int y){
-    return x + y * oled.getLCDWidth();
+    return x + y * getLCDWidth();
 }
 
 void getBallCount(){
-    oled.clear(PAGE);
-    oled.setCursor(5, 48/2 - 1 - 4);
-    oled.print("Xin chon");
-    oled.setCursor(5, 48/2 - 1 + 4);
-    oled.print("So banh");
-    oled.display();
-    usleep(1e6);
-    LCDHeightByBall = oled.getLCDHeight() / (2*BALL_RADIUS + 1); 
-    LCDWidthByBall = oled.getLCDWidth() / (2*BALL_RADIUS + 1);
+    cin >> ballCount;
+    LCDHeightByBall = getLCDHeight() / (2*BALL_RADIUS + 1); 
+    LCDWidthByBall = getLCDWidth() / (2*BALL_RADIUS + 1);
     LCDAreaByBall = LCDHeightByBall * LCDWidthByBall;
 
     bool agreePressed = false, pressFlag = false;
-    while(!agreePressed){
-        if (ballCount > 0 && BUTTON_DOWN.pinRead() == LOW && !pressFlag){
-            ballCount--;
-            pressFlag = 1;
-        }
-        else if (ballCount < LCDAreaByBall && BUTTON_UP.pinRead() == LOW && !pressFlag){
-            ballCount++;
-            pressFlag = 1;
-        }
-        else if(BUTTON_DOWN.pinRead() == HIGH && BUTTON_UP.pinRead() == HIGH){
-            pressFlag = 0;
-        }
-        if (ballCount && BUTTON_SELECT.pinRead() == LOW){
-            agreePressed = true;
-        }
-        oled.clear(PAGE);
-        oled.setCursor(0, 48/2 - 1);
-        oled.print(ballCount); oled.print(" banh");
-        oled.display();
+    while(ballCount > LCDAreaByBall){
+        cout << "Nhập lại thằng ngu" << endl;
+        cin >> ballCount;
     }
 }
 int getX(int flatIndex){
-    return flatIndex % oled.getLCDWidth();
+    return flatIndex % getLCDWidth();
 }
 int getY(int flatIndex){
-    return flatIndex / oled.getLCDWidth();
+    return flatIndex / getLCDWidth();
 }
 void initPos(){
     srand(time(NULL));
@@ -138,15 +105,15 @@ void initPos(){
         freeAreaIndex[j] = 1;
         int x = j % LCDWidthByBall * (2 * BALL_RADIUS + 1) + BALL_RADIUS,
             y = j / LCDWidthByBall * (2 * BALL_RADIUS + 1) + BALL_RADIUS;
-        listPos[i][0] = x;
-        listPos[i][1] = y;
+        listPos[i][0] = 0 + BALL_RADIUS;
+        listPos[i][1] = getLCDHeight() - BALL_RADIUS - 1;
         freeAreaByBall--;
     }
     delete[] freeAreaIndex;
 }
 
 void updateBoard(){
-    for (int i = 0; i < oled.getLCDHeight() * oled.getLCDWidth(); i++)
+    for (int i = 0; i < getLCDHeight() * getLCDWidth(); i++)
         boardBuffer[i] = 0;
     for(int i = 0; i < ballCount; i++){
         int8_t x0 = listPos[i][0], y0 = listPos[i][1];
@@ -197,54 +164,62 @@ void updateBoard(){
         boardBuffer[flatIndex(x0-radius, y0)] = i + OUTER_OFFSET;
     }
 }
-
+void clear(){
+    cout << "\033[2J\033[1;1H";
+}
 void drawingBoard(){
-    oled.clear(PAGE);
-    for (int i = 0; i < oled.getLCDHeight() * oled.getLCDWidth(); i++){
-        if (boardBuffer[i] < OUTER_OFFSET) continue;
-        oled.pixel(getX(i), getY(i));
+    clear();
+    for (int i = 0; i < getLCDHeight() * getLCDWidth(); i++){
+        if (i % getLCDWidth() == 0) cout << endl;
+        if (boardBuffer[i] < OUTER_OFFSET) cout << setw(1) << '-';
+        else cout << setw(1) << 'o';
     }
-    oled.display();
+    cout << '\n';
 }
 
 void movingBall(){
-    static int initFlag = 0;
-    if (!initFlag){
-        initFlag = 1;
-        imu = new LSM9DS0(0x6B, 0x1D);
-        uint16_t imuResult = imu->begin();
-        cout<<hex<<"Chip ID: 0x"<<imuResult<<dec<<" (should be 0x49d4)"<<endl;
-        return;
-    }
-    imu->newXData();
-    imu->readAccel();
-    float accelX = imu->calcAccel(imu->ax) * G_FORCE,
-          accelY = imu->calcAccel(imu->ay) * G_FORCE,
-          accelZ = imu->calcAccel(imu->ay) * G_FORCE;
+    // static int initFlag = 0;
+    // if (!initFlag){
+    //     initFlag = 1;
+    //     imu = new LSM9DS0(0x6B, 0x1D);
+    //     uint16_t imuResult = imu->begin();
+    //     cout<<hex<<"Chip ID: 0x"<<imuResult<<dec<<" (should be 0x49d4)"<<endl;
+    //     return;
+    // }
+    // imu->newXData();
+    // imu->readAccel();
+    float accelX = 0 * G_FORCE,
+          accelY = -1 * G_FORCE,
+          accelZ = 0 * G_FORCE;
     updateVelPos(accelX, accelY, accelZ);
 }
 
 void updateVelPos(float accelX, float accelY, float accelZ){
-    float total_accel = sqrt(pow(accelX,2) + pow(accelY,2) + pow(accelZ,2));
-    float total_accelXY =  sqrt(pow(accelX,2) + pow(accelY,2));
-    float inducedAccel = 1 - total_accel;
-    float inducedAccelXY = inducedAccel / total_accel * total_accelXY;
-    float inducedAccelX = inducedAccelXY / total_accelXY * accelX;
-    float inducedAccelY = inducedAccelXY / total_accelXY * accelY; 
-    accelX += inducedAccelX;
-    accelY += inducedAccelY;
-    //accelX = 10;
-    //accelY = 0;
+    // float total_accel = sqrt(pow(accelX,2) + pow(accelY,2) + pow(accelZ,2));
+    // float total_accelXY =  sqrt(pow(accelX,2) + pow(accelY,2));
+    // float inducedAccel = 1 - total_accel;
+    // float inducedAccelXY = inducedAccel / total_accel * total_accelXY;
+    // float inducedAccelX = inducedAccelXY / total_accelXY * accelX;
+    // float inducedAccelY = inducedAccelXY / total_accelXY * accelY; 
+    // accelX += inducedAccelX;
+    // accelY += inducedAccelY;
+    accelX = 0;
+    accelY = -10;
     for (int i = 0; i < ballCount; i++){
-        //if (!i) cout << accelX * listPos[i][0] + 1/2 * pow(listVel[i][0], 2) << endl;
+        if (!i) {
+            //cout << accelY << " " << listPos[i][1] << " " << listVel[i][1] << " >" << endl;
+            cout << accelY * (-listPos[i][1]) + 1./2 * pow(listVel[i][1], 2) << " <" << endl;
+        }
         float temp_vel_x = listVel[i][0] + accelX * DELTA_TIME_IN_US * 1e-6;
         float temp_vel_y = listVel[i][1] + accelY * DELTA_TIME_IN_US * 1e-6;
-        float vel = sqrt(pow(temp_vel_x, 2) + pow(temp_vel_y, 2));
-        //cout << "Vel1: " << temp_vel_x << " " << temp_vel_y << endl;
-        if (vel > VEL_CAP_IN_MS){
-            temp_vel_x = VEL_CAP_IN_MS * temp_vel_x / vel;
-            temp_vel_y = VEL_CAP_IN_MS * temp_vel_y / vel;
-        }
+        //cout << temp_vel_y << " vel" << listVel[i][1] << endl;
+        // cout << temp_vel_x << endl;
+            // float vel = sqrt(pow(temp_vel_x, 2) + pow(temp_vel_y, 2));
+        // //cout << "Vel1: " << temp_vel_x << " " << temp_vel_y << endl;
+        // if (vel > VEL_CAP_IN_MS){
+        //     temp_vel_x = VEL_CAP_IN_MS * temp_vel_x / vel;
+        //     temp_vel_y = VEL_CAP_IN_MS * temp_vel_y / vel;
+        // }
         // v0 * (t1 - t0) + 1/2 * (t1^2 - t0^2) = v0 * (t1 - t0) + 1/2 * (t1 - t0) * (t1 + t0)
         float temp_pos_x = listPos[i][0] - (listVel[i][0] * DELTA_TIME_IN_US * 1e-6 + 1./2 * accelX * pow(DELTA_TIME_IN_US, 2) * 1e-12);
         float temp_pos_y = listPos[i][1] + (listVel[i][1] * DELTA_TIME_IN_US * 1e-6 + 1./2 * accelY * pow(DELTA_TIME_IN_US, 2) * 1e-12);
@@ -252,14 +227,20 @@ void updateVelPos(float accelX, float accelY, float accelZ){
         float boundRight = temp_pos_x + BALL_RADIUS;
         float boundTop = temp_pos_y - BALL_RADIUS;
         float boundDown = temp_pos_y + BALL_RADIUS;
+        cout << boundLeft << " " << temp_pos_x << " " << temp_vel_x << " " << listPos[i][0] << " " << listVel[i][0] << endl;
+        //cout << listPos[i][1] << "/" << endl;
         if (boundLeft < 0){
+            // cout << pow(temp_vel_x,2) - 2 * accelX * 2*boundLeft << endl;
+            // if (pow(temp_vel_x,2) - 2 * accelX * 2*boundLeft < 0){
+            //     cout << "haha" << endl;
+            // }
             temp_vel_x = sqrt(POWER_LOSS_RATIO) * -sqrt(pow(temp_vel_x,2) - 2 * accelX * -boundLeft);
             boundLeft = 0;
             temp_pos_x = boundLeft + BALL_RADIUS;
         }
-        else if (boundRight > oled.getLCDWidth() - 1){
-            temp_vel_x = sqrt(POWER_LOSS_RATIO) * sqrt(pow(temp_vel_x, 2) - 2 * accelX * (oled.getLCDWidth() - 1 - boundRight));
-            boundRight = oled.getLCDWidth() - 1;
+        else if (boundRight > getLCDWidth() - 1){
+            temp_vel_x = sqrt(POWER_LOSS_RATIO) * sqrt(pow(temp_vel_x, 2) - 2 * accelX * (getLCDWidth() - 1 - boundRight));
+            boundRight = getLCDWidth() - 1;
             temp_pos_x = boundRight - BALL_RADIUS;
         }
         if (boundTop < 0){
@@ -267,23 +248,26 @@ void updateVelPos(float accelX, float accelY, float accelZ){
             boundTop = 0;
             temp_pos_y = boundTop + BALL_RADIUS;
         }
-        else if (boundDown > oled.getLCDHeight() - 1){
-            temp_vel_y = sqrt(POWER_LOSS_RATIO) * -sqrt(pow(temp_vel_y, 2) - 2 * accelY * -(oled.getLCDHeight() - 1 - boundDown));
-            boundDown = oled.getLCDHeight() - 1;
+        else if (boundDown > getLCDHeight() - 1){
+            //cout << 2*(getLCDHeight() - boundDown - 1) << endl;
+            temp_vel_y = sqrt(POWER_LOSS_RATIO) * -sqrt(pow(temp_vel_y, 2) - 2 * accelY * -(getLCDHeight() - 1 - boundDown));
+            boundDown = getLCDHeight() - 1;
             temp_pos_y = boundDown - BALL_RADIUS;
         }
+        cout  << boundLeft << " " << temp_pos_x << " " << temp_vel_x << " " << listPos[i][0] << " " << listVel[i][0] << endl;
         /*
         cout << ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" << endl;
         cout << "Accel: " << accelX << " " << accelY << endl;
         cout << "Pos0: "  << listPos[i][0] << " " << listPos[i][1] << endl;
         cout << "Vel0: " << listVel[i][0] << " " << listVel[i][1] << endl;
         cout << "Pos1: " << temp_pos_x << " " << temp_pos_y << endl;
-        cout << "Vel1: " << temp_vel_x << " " << temp_vel_y << endl;
+        cout << "Vel1: " << temp_vel_x << " " << temp_vel_y << endl;a.out
         cout << endl;*/
+        //cout << temp_vel_y << " vel" << listVel[i][1] << endl;
         listPos[i][0] = temp_pos_x;
         listPos[i][1] = temp_pos_y;
         listVel[i][0] = temp_vel_x;
         listVel[i][1] = temp_vel_y;
+        //cout << temp_vel_y << " vel" << listVel[i][1] << endl;
     }
 }
-    
